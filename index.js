326a5e4344f8553663b3f3b61d48221c5dff049f
josevsousa <jose.vicente.de.sousa>
2019-02-28 08:58:50 -0300

    feat: add search term and prefix

---
+++ index.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync')
+
+function start(){
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a wikipedia serach term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['jose', 'aldeir', 'caue']
+        const selectPrefixIndex = readline.keyInSelect(prefixes, 'Choose one options: ')
+        const selectPrefixTest = prefixes[selectPrefixIndex]
+        
+        return selectPrefixTest
+    }
+
+    console.log(content)
+}
+
+
+
+start()
